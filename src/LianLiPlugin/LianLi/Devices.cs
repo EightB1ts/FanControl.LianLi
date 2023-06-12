6279900849b0870ba0a75f4744cfc9ea8bb1335a
Cameron <me@cameronhalter.com>
2023-06-12 17:33:43 -0400

    Remove unused imports

--- src/LianLiPlugin/LianLi/Devices.cs
+++ src/LianLiPlugin/LianLi/Devices.cs
@@ -2,10 +2,6 @@
 using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using FanControl.LianLiPlugin;
-using HidSharp;
 
 namespace LianLi
 {
